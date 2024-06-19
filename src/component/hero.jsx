88a9725cd099e23df22888de5238e91993723b94
robiadiprawira <robiadiprawira@gmail.com>
2024-06-19 13:23:15 +0700

    Update

--- src/component/hero.jsx
+++ src/component/hero.jsx
@@ -3,13 +3,22 @@ import React from "react";
 const Hero = () => {
   return (
     <>
-      <div>
-        <div>Ready to Grow With Batara Guru Technology</div>
-        <div>
-          Discuss now and we help digitize your business goal with system
-          security guarantees to improve your company productivity
+      <div className="flex flex-col items-center py-120 gap-16 bg-hero-pattern bg-cover bg-no-repeat h-622">
+        <div className="w-[860px] flex flex-col gap-2">
+          <div className="text-5xl font-bold text-center leading-snug">
+            Ready to Grow With Batara Guru
+            <span className="block"> Technology</span>
+          </div>
+          <div className="text-center text-lg font-thin tracking-wider">
+            Discuss now and we help digitize your business goal with system
+            <span className="block">
+              security guarantees to improve your company productivity
+            </span>
+          </div>
         </div>
-        <button>Consultation Now</button>
+        <button className="bg-buttonCollor py-4 px-8 font-bold">
+          Consultation Now
+        </button>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,19 @@ module.exports = {
     extend: {
       colors: {
         primary: "#0E0E0D",
+        buttonCollor: "#A73848",
       },
-      fontFamily:{
-        'sans': ['Roboto Condensed', 'sans-serif'], 
+      fontFamily: {
+        sans: ["Roboto Condensed", "sans-serif"],
+      },
+      backgroundImage: {
+        "hero-pattern": "url(/src/img/bg-hero.png)",
+      },
+      height: {
+        622: "622px",
+      },
+      padding: {
+        120: "120px",
       },
     },
   },
